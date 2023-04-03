ac2735ad78bf064db1fd6fbe72f7004c44f11843
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-04-03 15:13:04 +0300

    change wrong name to img

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
         Hello world
       </h1>
       <h2>bye</h2>
-      <img src="https://www.thesprucepets.com/thmb/BSpyX1mL7mCnRibYEDlWnZv_4lM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/smartest-cat-breeds-4589754-hero-e192f849d9f1442481c47c932cd36aa8.jpg" alt="image"/>
+      <img src="https://www.thesprucepets.com/thmb/BSpyX1mL7mCnRibYEDlWnZv_4lM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/smartest-cat-breeds-4589754-hero-e192f849d9f1442481c47c932cd36aa8.jpg" alt="cat"/>
     </div>
   )
 }
