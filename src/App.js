e98427377e97f959e4d495cf8d4ae37fe9d9bc34
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-04-19 13:49:20 +0300

    changed errors added unworthy import

--- src/App.js
+++ src/App.js
@@ -1,65 +1,65 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Caregory from "./pages/Category";
 import NotFound from "./pages/NotFound";
 import { createContext, useEffect, useState } from "react";
 import { categoryCollection, productsCollection } from "./firebase";
 import { getDocs } from "firebase/firestore/lite";
-
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
 
 export const AppContext = createContext({
   categories: [],
-  products: []
+  products: [],
+  cart: {},
+  setCart: () => {},
 });
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
-   const [categories, setCategories] = useState([]);
-   const [products, setProducts] = useState([]);
-
-   useEffect(() => {
-     getDocs(categoryCollection)
-    .then(({ docs }) => {
+  useEffect(() => {
+    getDocs(categoryCollection).then(({ docs }) => {
       setCategories(
-        docs.map(doc => ({
+        docs.map((doc) => ({
           ...doc.data(),
-          id: doc.id
+          id: doc.id,
         }))
-      )
+      );
     });
 
-    getDocs(productsCollection)
-    .then(({ docs }) => {
+    getDocs(productsCollection).then(({ docs }) => {
       setProducts(
-        docs.map(doc => ({
+        docs.map((doc) => ({
           ...doc.data(),
-          id: doc.id
+          id: doc.id,
         }))
-      )
+      );
     });
-
-   }, []);
+  }, []);
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories, products }}>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<h2>Home</h2>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/contacts" element={<h1>Contacts</h1>} />
-          <Route path="/deliver" element={<h1>Deliver</h1>} />
-          <Route path="/categories/:slug" element={<Caregory />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <AppContext.Provider value={{ categories, products, cart, setCart }}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="/about" element={<h1>About</h1>} />
+            <Route path="/contacts" element={<h1>Contacts</h1>} />
+            <Route path="/deliver" element={<h1>Deliver</h1>} />
+            <Route path="/categories/:slug" element={<Caregory />} />
+            <Route path="/products/:slug" element={<Product />} />
 
-          
-
-       </Layout>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
       </AppContext.Provider>
     </div>
-  )
+  );
 }
 
 export default App;
