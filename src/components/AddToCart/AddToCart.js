bc1cea3535bcbfc842aeb43265fc0a5a7bcfa666
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-04-19 13:50:52 +0300

    Added AddToCart

---
+++ src/components/AddToCart/AddToCart.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import "./AddToCart.css";
+import { AppContext } from "../../App";
+
+export default function AddToCart({ product }) {
+  const { cart, setCart } = useContext(AppContext);
+
+  function onAddToCartClick() {
+    const qty = cart[product.id] ? cart[product.id] + 1 : 1;
+    setCart({
+      ...cart,
+         [product.id]: qty, 
+    });
+  }
+
+  return (
+    <div className="AddToCart">
+      {cart[product.id] ? cart[product.id] : 0}
+      <button onClick={onAddToCartClick}>Add to cart</button>
+    </div>
+  );
+}
