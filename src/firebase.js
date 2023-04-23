18f727c0a6e59940ce323adf8e651219efeed0bc
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-04-23 21:30:09 +0300

    added to firebase getAuth

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getFirestore, collection } from 'firebase/firestore/lite'
+import { initializeApp } from "   /app";
+import { getFirestore, collection } from 'firebase/firestore/lite'
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +18,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export const categoryCollection = collection(db, 'categories')
+export const categoryCollection = collection(db, 'categories');
 export const productsCollection = collection(db, 'products');
+export const ordersCollection = collection(db, 'orders');
+
+const provider = new GoogleAuthProvider();
+export const signIn = () => signInWithPopup(auth, provider);
+export const signOff = () => signOut(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
