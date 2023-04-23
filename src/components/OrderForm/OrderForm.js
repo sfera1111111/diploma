83b85428417f01f06f8b388a56dd667959932dba
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-04-23 21:36:11 +0300

    added order form

---
+++ src/components/OrderForm/OrderForm.js
@@ -0,0 +1,52 @@
+import { addDoc } from "firebase/firestore/lite";
+import "./OrderForm.css";
+import { ordersCollection } from "../../firebase";
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { useNavigate } from "react-router-dom";
+
+export default function OrderForm() {
+  const { cart, setCart } = useContext(AppContext);
+  const navigator = useNavigate
+
+  if (Object.keys(cart).length === 0) {
+    return "Your cart is empty.";
+  }
+  
+  function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    addDoc(ordersCollection, {
+      name: formData.get('name'),
+      phone: formData.get('phone'),
+      email: formData.get('email'),
+      address: formData.get('address'),
+      cart: cart,
+    })
+    .then(doc => {
+      setCart({});
+      navigator('/thank-you');
+    })
+  }
+
+  return (
+    <form className="OrderForm" onSubmit={onFormSubmit}>
+      <h2>Create an order</h2>
+      <label>
+        Name: <input type="text" name="name" required />
+      </label>
+      <label>
+        Phone: <input type="tel" name="phone" required />
+      </label>
+      <label>
+        Email: <input type="email" name="email" required />
+      </label>
+      <label>
+        Address: <input type="text" name="address" required />
+      </label>
+      <button>Submit</button>
+    </form>
+  );
+}
