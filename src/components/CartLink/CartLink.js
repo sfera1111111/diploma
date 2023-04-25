e582378b3b02ea0069b9aa5a67d383339ea91b0c
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-04-26 01:04:16 +0300

    added cart to CartLink

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -7,14 +7,13 @@ import CartList from "../CartList/CartList";
 export default function CartLink() {
   const { cart } = useContext(AppContext);
 
-  // вычислить кол-во товаров
   const total = Object.values(cart).reduce(
     (acc, num) => acc + num, 0
   );
 
   return (
     <div className="CartLink">
-      <NavLink to="/cart">
+      <NavLink to="/cart" className="cart">
         Cart ({total})
       </NavLink>
     </div>
