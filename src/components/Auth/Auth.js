1c36a72d2ff8de758664203d371bac72033bec60
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-04-26 01:05:56 +0300

    added logIn and logOut and user

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,16 +1,24 @@
-import { signOut } from "firebase/auth";
-import { signIn } from "../../firebase";
+import { logIn, logOut } from "../../firebase";
 import { AppContext } from "../../App";
 import { useContext } from "react";
+import "./Auth.css";
+import { Link } from "react-router-dom";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
 
   return (
     <div className="Auth">
-      <button onClick={signIn}>Sing in</button>
-      {user ? <span>{user.displayName}</span> : null}
-      {user ? <button onClick={signOut}>Sign out</button> : null} 
-    </div>
-  );
-}
+      {user ? (
+        <span>
+          Hello <Link to="/orders">{user.displayName}</Link>!
+          <button onClick={logOut}>Sign out</button>
+        </span>
+      ) : (
+        <span>
+          Hello guest!
+          <button onClick={logIn}>Sign in</button>
+        </span>
+      )}
+     </div>
+)}
