3b8c4bcfefdda43252d8d8dca7526a551d6a648b
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-01 15:35:54 +0300

    added Search

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,21 +3,25 @@ import Logo from "../Logo/Logo";
 import CartLink from "../CartLink/CartLink";
 import CategoryList from "../CategoryList/CategoryList";
 import Auth from "../Auth/Auth";
+import Search from "../Search/Search";
 import "./Layout.css";
 
 export default function Layout(props) {
   return (
     <div className="layout">
        <header>
+        <div className="diplay">
         <Logo />
-        <Nav />
+        <Nav  />
         <CartLink className="cartlink" />
         <Auth />
+        </div> 
+        <Search />  
        </header>
        <aside>
         <CategoryList />      
        </aside>
-       <main>{props.children}</main>
+       <main>{children}</main>
        <footer>FOOTER</footer>
     </div>
   );
