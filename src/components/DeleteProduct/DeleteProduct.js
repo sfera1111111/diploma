a1958fd214993a9e51c529eb074fc0b2b54fd8e6
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-11 13:41:05 +0300

    Added DleteProduct and AddProduct

---
+++ src/components/DeleteProduct/DeleteProduct.js
@@ -0,0 +1,35 @@
+import { deleteObject, ref } from "firebase/storage";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db, storage } from "../../firebase";
+import { useContext } from "react";
+import { AppContext } from "../../App";
+
+
+export default function DeleteProduct({ product }) {
+  const { user } = useContext(AppContext);
+
+  if (!user || !user.isAdmin) {
+    return null;
+  }
+
+  function onDeleteClick() {
+    if (!window.confirm("Are you sure you want to delete this product?")) {
+      return;
+    }
+
+    const picture = ref(storage, product.picture);
+    deleteObject(picture)
+      .then(() => {
+        deleteDoc(doc(db, "products", product.id));
+      })
+      .catch((error) => {
+        console.log("Failed to delete product image:", error);
+      });
+  }
+
+  return (
+    <button className="DeleteProduct" onClick={onDeleteClick}>
+      -
+    </button>
+  );
+}
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, onSnapshot } from 'firebase/firestore'
 import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const auth = getAuth(app);
+export const storage = getStorage(app);
 
 export const categoryCollection = collection(db, 'categories');
 export const productsCollection = collection(db, 'products');
@@ -58,3 +60,11 @@ export const onOrdersLoad = (callback) =>
       }))
     )
   );
+
+  export const uploadProductPhoto = async (file) => {
+    const storageRef = ref(storage, `products/${file.name}`);
+    await uploadBytes(storageRef, file);
+  
+    const url = await getDownloadURL(storageRef);
+    return url;
+  };
