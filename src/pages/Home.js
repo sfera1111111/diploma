98447b0384099c7077295b39591cce473fda2eb6
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-12 13:59:21 +0300

    Added text aling to layout,css and Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ export default function Home () {
   return (
     <div className="home">
       <h1>Home</h1>
-      <p>This is my home page</p>
+      <p className="text-aling">This is my home page</p>
       <p> Kyrgyzstan. For our partners and buyers, there is a comprehensive lending program, the purchase of goods by installments both with the services of banks, and without the participation of a bank and without confirmation of income, and payment by installments at the place of delivery of the order.<br />
       Kyrgyzstan. For our partners and buyers, there is a comprehensive lending program, the purchase of goods by installments both with the services of banks, and without the participation of a bank and without confirmation of income, and payment by installments at the place of delivery of the order.<br />
      Kyrgyzstan. For our partners and buyers, there is a comprehensive lending program, the purchase of goods by installments both with the services of banks, and without the participation of a bank and without confirmation of income, and payment by installments at the place of delivery of the order.<br />
