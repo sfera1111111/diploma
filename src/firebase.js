71b17237f7e1f169e764f1791de47e263346e9c6
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-16 14:02:51 +0300

    Added firebase to export uploadProductPhoto

--- src/firebase.js
+++ src/firebase.js
@@ -61,10 +61,13 @@ export const onOrdersLoad = (callback) =>
     )
   );
 
-  export const uploadProductPhoto = async (file) => {
+  export const uploadProductPhoto = (file) => {
     const storageRef = ref(storage, `products/${file.name}`);
-    await uploadBytes(storageRef, file);
-  
-    const url = await getDownloadURL(storageRef);
-    return url;
+    return uploadBytes(storageRef, file)
+      .then(() => {
+        return getDownloadURL(storageRef);
+      })
+      .catch((error) => {
+        console.log("Failed to upload product photo:", error);
+      });
   };
