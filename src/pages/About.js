6ee7a1f7adf4c36f4ea975a7a5b7a98ff23efa3b
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-16 23:18:58 +0300

    Added text to About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,13 @@
-export default function About() {
+export default function About () {
   return (
-    <h1>About</h1>
+    <div>
+      <h1>About</h1>
+      <p>
+      The company "TECHNOPOLIS" was approved in 2011 and today it is successfully advancing in the market of <b>household appliances in Bishkek</b> and in the regions of <b>Kyrgyzstan</b>. Dynamically developing in the field of <b>wholesale</b> and <b>retail sales</b>, our close-knit team has earned the trust of customers and partners for reliability and punctuality.<br />
+      The team of professionals of our company offers the widest and constantly updated range of goods <b>household appliances</b>, electrical goods at <b>the lowest prices</b>.<br />
+      We provide <b>free shipping on orders</b> and ship to all regions Kyrgyzstan. For our partners and buyers, there is a comprehensive <b>loan program</b>, purchase of goods in <b>installments</b> without the participation of a bank and without confirmation of income, payment by installments on the spot or at the delivery address. An attractive <b>discount system</b> and a customer incentive program are constantly operating, <b>promotions</b> and <b>sales</b>.<br />
+      We are always grateful to our customers and appreciate each of them. We invite you to become our partner!
+      </p>
+    </div>
   );
 }
