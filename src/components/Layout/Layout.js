35550237dabdd540b3f2166715537fd748c26e3f
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-16 23:12:53 +0300

    Added Wrapper to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,6 +8,7 @@ import Drawer from "../Drawer/Drawer";
 import  Search  from "../Search/Search";
 import "./layout.css";
 import { useState } from "react";
+import Wrapper from "../Wrapper/Wrapper";
 
 export default function Layout(props) {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -19,10 +20,12 @@ export default function Layout(props) {
     <div className="layout">
        <header>
          <div className="header__body">
+          <div className="flex">
         <Logo />
         <Nav />
         <NavToggle callback={toggleDrawer} />
         <Drawer open={drawerOpen} toggle={toggleDrawer} />
+        </div>
         <div className="mg_l">
         <Search />
         <Auth />
@@ -34,7 +37,10 @@ export default function Layout(props) {
         <CategoryList  className="CategoryList"/>      
        </aside>
        <main>{props.children}</main>
-       <footer>FOOTER</footer>
+       <footer>
+        footer
+        <Wrapper />
+        </footer>
     </div>
   );
 }
