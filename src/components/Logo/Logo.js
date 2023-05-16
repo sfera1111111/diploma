36b3f079385d3baf86c9f830e896b547b2c2db5e
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-16 23:16:12 +0300

    changed logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,11 @@
 import "./Logo.css";
-import logo from "../../assets/logo.webp";
+import logo from "../../assets/Logo.png";
 
 
 export default function Logo() {
   return (
      <div className="Logo">
-       
-       <img src={logo} alt="Nologo" className="logo" />
-      
+       <img src={logo} alt="Nologo" className="logo" />      
      </div>
   )
 }
