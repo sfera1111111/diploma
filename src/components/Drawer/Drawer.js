4999f8049f87f4f5d99761a83b48e814adee291f
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-19 00:51:16 +0300

    Added  Search to  Drawer

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import Search from "../Search/Search";
 import "./Drawer.css"
 
 function Drawer({ open, toggle }) {
@@ -9,7 +10,7 @@ function Drawer({ open, toggle }) {
     <div className={drawerClassNames}>
       <div onClick={toggle} className="backdrop"></div>
       <div className="content">
-        <Logo />
+        <Search />
         <Nav />
       </div>
     </div>
