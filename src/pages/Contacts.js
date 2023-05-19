39656e598a5dbc27dae5cae6266c20abe7a24b86
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-19 14:11:18 +0300

    Added Contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,7 +1,10 @@
+import { NavLink } from "react-router-dom";
+
 export default function Contacts () {
   return (
     <div className="Contacts">
-      <h6 className="fs">+996504 070 731</h6>
-    </div>
+      <h6 className="fs">E-mail: <NavLink>samarbekjymakadurov@gmail.com</NavLink></h6>
+      <h6 className="fs">Phone: <NavLink>+996504 070 731</NavLink></h6>
+      </div>
   );
 }
