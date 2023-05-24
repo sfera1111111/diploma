739a3e14edef3d67971c916500b06aecef3b0068
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-05-24 15:21:39 +0300

    added text to Delivery

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -4,8 +4,14 @@ export default function Delivery () {
   return (
     <div className="Delivery">
       <h1>Delivery and pickup of goods</h1>
-      <p>1:<NavLink>Pickup</NavLink></p>
-      <p>2: <NavLink>international delivery</NavLink></p>
+      <p>An order can be placed in several ways.</p>
+      <p>Выберите товар и добавьте его в корзину, нажав клавишу «В корзину» или «по WhatsApp» рядом с товаром.
+         <br />Зайдите в Корзину в верхнем правом углу сайта.
+         Нажмите клавишу «Оформить заказ».<br />
+         Введите имя, номер телефона и адрес доставки (дополнительный адрес, если необходимо).<br />
+         Выберите способ оплаты.<br />
+         Выберите способ доставки.<br />
+        Нажмите клавишу «Подтвердить заказ» и ожидайте звонка оператора (до 15 мин).</p>
     </div>
   );
 }
